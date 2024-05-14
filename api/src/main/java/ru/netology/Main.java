361f72e6a4752ecfc782d834956ddccb7c369a1f
AlexeyPanichev <159615024+AlexeyPanichev@users.noreply.github.com>
2024-05-14 15:17:26 +0300

    Initial commit

---
+++ api/src/main/java/ru/netology/Main.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+    public class Main {
+
+        public static void main(String[] args) {
+            MyService myService = new MyService();
+            System.out.println(myService.getMyEntity());
+            System.out.println(myService.setMyEntity(new MyEntity("second")));
+            System.out.println(myService.getMyEntity());
+        }
+    }
